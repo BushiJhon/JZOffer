dbb3ae956b2649ffaac2934ed20f1564dc6b6bcd
BushiJhon <1172905107@qq.com>
2020-06-08 18:26:17 +0800

    剑指第八题

---
+++ JZ8.java
@@ -0,0 +1,14 @@
+//思路：本质是斐波那契数列
+
+public class Solution {
+    public int JumpFloor(int target) {
+        if(target <= 0)
+            return 0;
+        else if(target == 1)
+            return 1;
+        else if(target == 2)
+            return 2;
+        
+        return JumpFloor(target-1) + JumpFloor(target-2);
+    }
+}
